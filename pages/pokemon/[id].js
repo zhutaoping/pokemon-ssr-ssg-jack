ef6c50e95f2acfefeed9234af570f06989111665
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-10-19 10:18:54 +0800

    client side rendering

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,75 @@
+/* eslint-disable @next/next/no-img-element */
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import styles from "../../styles/Details.module.css";
+
+const Details = () => {
+	const {
+		query: { id },
+	} = useRouter();
+
+	const [pokemon, setPokemon] = useState(null);
+
+	useEffect(() => {
+		async function getPokemon() {
+			console.log("test");
+			const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+			const data = await res.json();
+			setPokemon(data);
+		}
+		if (id) getPokemon();
+	}, [id]);
+
+	if (!pokemon) {
+		return null;
+		s;
+	}
+
+	return (
+		<div>
+			<Head>
+				<title>{pokemon.name}</title>
+			</Head>
+			<div>
+				<Link href="/">
+					<a>Back to Home</a>
+				</Link>
+			</div>
+			<div className={styles.layout}>
+				<div>
+					<img
+						className={styles.picture}
+						src={pokemon.sprites.other["official-artwork"].front_default}
+						alt={pokemon.name.english}
+					/>
+				</div>
+				<div>
+					<div className={styles.name}>{pokemon.name}</div>
+					<div className={styles.type}>
+						{pokemon.types.map((type) => type.type.name).join(", ")}
+					</div>
+					<table>
+						<thead className={styles.heading}>
+							<tr>
+								<th>Name</th>
+								<th>Value</th>
+							</tr>
+						</thead>
+						<tbody>
+							{pokemon.stats.map((st) => (
+								<tr key={st.stat.name}>
+									<td className={styles.attribute}>{st.stat.name}</td>
+									<td>{st.base_stat}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Details;
